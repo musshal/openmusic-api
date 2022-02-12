2112ef23b35264c516657a02e6827bf0a1bf77f1
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-12 11:43:42 +0700

    Creating deleteAlbumByIdHandler method

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -73,6 +73,28 @@ class AlbumsHandler {
       return response;
     }
   }
+
+  deleteAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      this._service.deleteAlbumById(id);
+
+      return {
+        status: 'success',
+        message: 'Album berhasil dihapus',
+      };
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+
+      response.code(404);
+
+      return response;
+    }
+  }
 }
 
 module.exports = AlbumsHandler;
