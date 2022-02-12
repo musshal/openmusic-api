fb9691d571d580e7ffa315e52c356c64ab13ccfd
Musthafa Faishal <musthafafaishal@gmail.com>
2022-02-13 05:04:27 +0700

    Creating migrations

---
+++ migrations/1644702503351_create-table-songs.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER(50)',
+      notNull: false,
+    },
+    albumId: 'VARCHAR(50)',
+    notNull: false,
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
