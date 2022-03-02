ecf5451fbeb3299a9b80eef0d39a6cef955f2887
Musthafa Faishal <musthafafaishal@gmail.com>
2022-03-03 04:02:13 +0700

    Creating addRefreshToken function

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -4,6 +4,15 @@ class AuthenticationsService {
   constructor() {
     this._pool = new Pool();
   }
+
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
 }
 
 module.exports = AuthenticationsService;
