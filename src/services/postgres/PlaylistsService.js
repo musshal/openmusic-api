9619c72fea478da3be8aa23f9570846f70377baf
Musthafa Faishal <musthafafaishal@gmail.com>
2022-03-03 19:07:13 +0700

    Creating addPlaylist function

--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -1,8 +1,27 @@
 const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class PlaylistsService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addPlaylist(name) {
+    const id = `playlist-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO playlists VALUES($1, $2) RETURNING id',
+      values: [id, name],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Playlist gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
 
