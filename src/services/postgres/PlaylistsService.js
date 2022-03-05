725c44f4b7f51e43d39481dc1037cb14c3fbe527
Musthafa Faishal <musthafafaishal@gmail.com>
2022-03-05 19:09:50 +0700

    Creating addSongToPlaylist function

--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -52,6 +52,22 @@ class PlaylistsService {
       throw new NotFoundError('Playlist gagal dihapus. Id tidak ditemukan');
     }
   }
+
+  async addSongToPlaylist(songId, playlistId) {
+    const id = `playlist_songs-${nanoid(16)}`;
+    const query = {
+      text: 'INSERT INTO playlist_songs VALUES($1, $2, $3) RETURNING id',
+      values: [id, songId, playlistId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Lagu gagal ditambahkan ke dalam playlist');
+    }
+
+    return result.rows[0].id;
+  }
 }
 
 module.exports = PlaylistsService;
