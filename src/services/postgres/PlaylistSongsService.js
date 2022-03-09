a171269a75c467aa3278dc95915cc58c4f80317b
Musthafa Faishal <musthafafaishal@gmail.com>
2022-03-09 10:52:00 +0700

    Creating verifyCollaborator function and changing param name

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -22,10 +22,10 @@ class PlaylistSongsService {
     }
   }
 
-  async getSongsFromPlaylist(id) {
+  async getSongsFromPlaylist(playlistId) {
     const query = {
       text: 'SELECT songs.id, songs.title, songs.performer FROM songs LEFT JOIN playlist_songs ON songs.id = playlist_songs.song_id WHERE playlist_songs.playlist_id = $1',
-      values: [id],
+      values: [playlistId],
     };
 
     const result = await this._pool.query(query);
