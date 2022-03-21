37af3db519f6f173d2c51281a11ac25c9f771f0a
Musthafa Faishal <musthafafaishal@gmail.com>
2022-03-21 18:45:27 +0700

    final update

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -17,7 +17,7 @@ class ExportsHandler {
     await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
 
     const message = {
-      userId: request.auth.credentials.id,
+      playlistId,
       targetEmail: request.payload.targetEmail,
     };
 
